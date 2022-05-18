2e436e065517f3f888d1f6ca606db822c4ed0fd9
Jin <lycanjin@gmail.com>
2022-05-18 17:13:47 +0900

    Jin 1. VO 클래스 추가

---
+++ app/src/main/java/com/asianaidt/edu/orderapp_20220518/datas/StoreData.kt
@@ -0,0 +1,10 @@
+package com.asianaidt.edu.orderapp_20220518.datas
+
+class StoreData(
+    val name : String,
+    val logoURL : String,
+    val score : Double,
+    val phoneNum : String
+) {
+
+}
